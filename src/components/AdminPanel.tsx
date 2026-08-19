c6d0182c07269bfbcc3180f317327d3ad94d03e0
Felipe Alcantar <felilialcantar.11@gmail.com>
2026-08-19 11:10:02 -0600

    Edit2 import

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -751,13 +751,13 @@ export const AdminPanel: React.FC<AdminPanelProps> = ({
                               >
                                 <MessageCircle className="w-4 h-4" />
                               </a>
-                              {/* <button
+                              <button
                                 onClick={() => handleStartEdit(rsvp)}
                                 className="p-2 text-gray-400 hover:text-red-600 hover:bg-red-50 rounded transition-colors"
                                 title="Editar invitado"
                               >
                                 <Edit2 className="w-4 h-4" />
-                              </button> */}
+                              </button>
                               <button
                                 onClick={() => handleDeleteRSVP(rsvp.id)}
                                 className="p-2 text-gray-400 hover:text-red-600 hover:bg-red-50 rounded transition-colors"
