a50e67d10f07778350129bc25b6019b1c327b625
Felipe Alcantar <felilialcantar.11@gmail.com>
2026-08-27 14:43:50 -0600

    subtitulo

--- src/App.tsx
+++ src/App.tsx
@@ -238,7 +238,7 @@ export default function App() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="font-script text-3xl sm:text-4xl mb-8 text-silver-light animate-pulse-soft mt-4"
+            className="font-script text-3xl sm:text-4xl mb-8 text-night-soft animate-pulse-soft mt-4"
           >
             {eventDetails.subtitle}
           </motion.p>
